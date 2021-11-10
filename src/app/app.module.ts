35dd05e704acd606682a76a25feb741ab42d0a1c
DevDoge <lhuthefranc@hotmail.com>
2021-11-10 17:13:02 -0300

    Criação dos Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    WrapperComponent
   ],
   imports: [
     BrowserModule
