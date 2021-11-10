08a37bc2c679e7399bd65b225a6bd6e4e1a76fe7
DevDoge <lhuthefranc@hotmail.com>
2021-11-10 18:14:25 -0300

    Criação do sidebar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { WrapperComponent } from './wrapper/wrapper.component';
+import { SidebarComponent } from './header/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    WrapperComponent
+    WrapperComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
