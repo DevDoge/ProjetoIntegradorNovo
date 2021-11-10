00764e3bc681b457fc713c868cef49d77b3b6bdf
DevDoge <lhuthefranc@hotmail.com>
2021-11-10 18:32:27 -0300

    Criação do dashboard component e account component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './header/header.component';
 import { WrapperComponent } from './wrapper/wrapper.component';
 import { SidebarComponent } from './header/sidebar/sidebar.component';
 import { TopbarComponent } from './header/topbar/topbar.component';
+import { DashboardComponent } from './wrapper/dashboard/dashboard.component';
+import { AccountComponent } from './wrapper/account/account.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { TopbarComponent } from './header/topbar/topbar.component';
     HeaderComponent,
     WrapperComponent,
     SidebarComponent,
-    TopbarComponent
+    TopbarComponent,
+    DashboardComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule
